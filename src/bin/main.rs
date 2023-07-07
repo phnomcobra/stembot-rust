1765b72cd7492993b6173349970cd60f094aa008
Justin Dierking <justin.dierking@Justins-MacBook-Pro.local>
2023-07-06 21:02:24 -0400

    actix boilerplating and scheduler updating

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,7 @@
-use std::time::Duration;
-
+use actix_web::{http::Error, rt::spawn, web, App, HttpResponse, HttpServer, Result};
 use clokwerk::{AsyncScheduler, Interval::Seconds};
+use futures_util::StreamExt;
+use std::time::Duration;
 use tokio::time::sleep;
 
 use stembot_rust::{config::Configuration, init_logger};
@@ -9,8 +10,20 @@ async fn schedule_test() {
     log::info!("scheduler test");
 }
 
-#[tokio::main]
-async fn main() {
+async fn index(mut payload: web::Payload) -> Result<HttpResponse, Error> {
+    let mut body = web::BytesMut::new();
+
+    while let Some(chunk) = payload.next().await {
+        let chunk = chunk?;
+
+        body.extend_from_slice(&chunk);
+    }
+
+    Ok(HttpResponse::Ok().body(body))
+}
+
+#[actix_web::main]
+async fn main() -> Result<(), std::io::Error> {
     if std::env::var("RUST_BACKTRACE").is_err() {
         std::env::set_var("RUST_BACKTRACE", "1");
     }
@@ -27,8 +40,16 @@ async fn main() {
 
     scheduler.every(Seconds(1)).run(schedule_test);
 
-    loop {
-        scheduler.run_pending().await;
-        sleep(Duration::from_millis(10)).await;
-    }
+    spawn(async move {
+        loop {
+            scheduler.run_pending().await;
+            sleep(Duration::from_millis(10)).await;
+        }
+    });
+
+    log::info!("Starting webserver...");
+    HttpServer::new(|| App::new().route("/", web::post().to(index)))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }

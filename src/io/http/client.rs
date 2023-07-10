4314fcd4ab20b73b14856d2c341264cbf2cea20d
Justin Dierking <phnomcobra@gmail.com>
2023-07-09 23:54:38 -0400

    proper error handling for http client

--- src/io/http/client.rs
+++ src/io/http/client.rs
@@ -9,7 +9,7 @@ use crate::config::Configuration;
 pub async fn send_message<T: Into<Vec<u8>>>(message: T) -> Result<Vec<u8>, Box<dyn Error>> {
     let configuration = Configuration::new_from_cli();
 
-    let body_as_bytes: Vec<u8> = message.into();
+    let unencrypted_request_body: Vec<u8> = message.into();
 
     // Harden this to filter out empty tokens between the host and endpoint
     let url = format!(
@@ -19,61 +19,68 @@ pub async fn send_message<T: Into<Vec<u8>>>(message: T) -> Result<Vec<u8>, Box<d
 
     let client = Client::new();
 
-    let mut nonce = sha256::digest(rand::random::<[u8; 32]>().as_ref());
+    let nonce_string = sha256::digest(rand::random::<[u8; 32]>().as_ref());
 
-    let mut cipher = new_magic_crypt!(&configuration.secret, 256, &nonce);
+    let cipher = new_magic_crypt!(&configuration.secret, 256, &nonce_string);
 
-    let mut tag = sha256::digest(body_as_bytes.as_slice());
+    let tag_string = sha256::digest(unencrypted_request_body.as_slice());
 
-    let b64_request_body = cipher.encrypt_bytes_to_base64(body_as_bytes.as_slice());
+    let encrypted_request_body = cipher.encrypt_bytes_to_base64(unencrypted_request_body.as_slice());
 
     match client
         .post(url)
-        .body(b64_request_body)
-        .header("Nonce", nonce)
-        .header("Tag", tag)
+        .body(encrypted_request_body)
+        .header("Nonce", nonce_string)
+        .header("Tag", tag_string)
         .send()
         .await
     {
         Ok(response) => match response.status() {
             StatusCode::OK => {
-                nonce = String::from(
-                    response
-                        .headers()
-                        .get("Nonce")
-                        .expect("nonce missing from response header")
-                        .to_str()
-                        .expect("failed to read nonce from response header"),
-                );
-                tag = String::from(
-                    response
-                        .headers()
-                        .get("Tag")
-                        .expect("tag missing from response header")
-                        .to_str()
-                        .expect("failed to read tag from response header"),
-                );
+                let nonce_header_value = match response.headers().get("Nonce") {
+                    Some(value) => value,
+                    None => return Err("nonce missing from response headers".into()),
+                };
 
-                cipher = new_magic_crypt!(&configuration.secret, 256, &nonce);
+                let nonce_string = match nonce_header_value.to_str() {
+                    Ok(value) => String::from(value),
+                    Err(_) => return Err("failed to read nonce from response header".into()),
+                };
 
-                let response_b64body = response
+                let tag_header_value = match response.headers().get("Tag") {
+                    Some(value) => value,
+                    None => return Err("tag missing from response headers".into()),
+                };
+
+                let tag_string = match tag_header_value.to_str() {
+                    Ok(value) => String::from(value),
+                    Err(_) => return Err("failed to read tag from response header".into()),
+                };
+
+                let encrypted_response_body_bytes = match response
                     .bytes()
-                    .await
-                    .expect("failed to receive http response body");
-                let response_body = cipher
-                    .decrypt_base64_to_bytes(
-                        String::from_utf8(response_b64body.to_vec())
-                            .expect("failed to read http response body"),
-                    )
-                    .expect("failed to decrypt http response body");
+                    .await {
+                        Ok(bytes) => bytes.to_vec(),
+                        Err(_) => return Err("failed to receive encrypted http response body".into()),
+                    };
 
-                assert_eq!(
-                    tag,
-                    sha256::digest(response_body.as_slice()),
-                    "http response body digest mismatch"
-                );
+                let encrypted_response_body_string = match String::from_utf8(encrypted_response_body_bytes) {
+                    Ok(string) => string,
+                    Err(_) => return Err("failed to read encrypted response body".into()),
+                };
 
-                Ok(response_body)
+                let cipher = new_magic_crypt!(&configuration.secret, 256, &nonce_string);
+
+                let decrypted_response_body = match cipher
+                    .decrypt_base64_to_bytes(encrypted_response_body_string) {
+                        Ok(bytes) => bytes,
+                        Err(_) => return Err("failed to decrypt http response body".into())
+                    };
+
+                match tag_string == sha256::digest(decrypted_response_body.as_slice()) {
+                    true => Ok(decrypted_response_body),
+                    false => Err("http response body digest mismatch".into()),
+                }
             }
             _ => Err(format!("HTTP Status: {}", response.status()).into()),
         },
